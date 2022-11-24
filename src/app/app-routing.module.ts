f4c4e813990459a209f509544e6af54ed3622268
Mario Torres <mario.torreslepe@gmail.com>
2022-11-24 12:24:24 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {PagesRoutingModule} from './pages/pages-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'/home', pathMatch: 'full' },
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
