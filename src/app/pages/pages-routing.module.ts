a323f22456c3086ed27abc0e416be9e6b07825e2
Mario Torres <mario.torreslepe@gmail.com>
2022-11-25 10:56:51 -0300

    fix modules project

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AddGroupComponent} from './add-group/add-group.component';
-import {AddNoteComponent} from './add-note/add-note.component';
 import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages.component';
 
@@ -11,8 +9,6 @@ const routes: Routes = [
     component: PagesComponent,
     children: [
       { path: '', component: HomeComponent },
-      { path: 'add-note', component: AddNoteComponent },
-      { path: 'add-group', component: AddGroupComponent },
     ],
   },
 ];
