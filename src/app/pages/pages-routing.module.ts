42ca7f78e19267dd4c5d3ce557336ed0d89974ad
Mario Torres <mario.torreslepe@gmail.com>
2022-11-24 23:58:12 -0300

    add components to add notes ands andd group

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddGroupComponent} from './add-group/add-group.component';
+import {AddNoteComponent} from './add-note/add-note.component';
 import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages.component';
 
@@ -7,7 +9,11 @@ const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'add-note', component: AddNoteComponent },
+      { path: 'add-group', component: AddGroupComponent },
+    ],
   },
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import {SharedModule} from '../shared/shared.module';
 import {RouterModule} from '@angular/router';
+import { AddGroupComponent } from './add-group/add-group.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 
 
 @NgModule({
   declarations: [
-    PagesComponent
+    PagesComponent,
+    AddGroupComponent,
+    AddNoteComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule
-  ] 
+  ]
 })
 export class PagesModule { }
